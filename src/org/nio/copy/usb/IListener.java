2f59aadfd15d6b51453fb0d5807a1f9c704ea329
alex.liu <alex.liu@nio.com>
2019-03-20 14:21:36 +0800

    Clear the repo

--- src/org/nio/copy/usb/IListener.java
+++
@@ -1,9 +0,0 @@
-package org.nio.copy.usb;
-
-public interface IListener {
-    void onStart();
-    void onComplete();
-    void onError();
-    void onCancel();
-    void onProgress(long cur, long total);
-}
